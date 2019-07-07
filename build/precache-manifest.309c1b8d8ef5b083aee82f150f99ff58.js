82ff80bbb7f153e2c6831c196d24dde9a81dc761
Nityashasinha1999 <nityashasinha1999@gmail.com>
2019-07-08 01:16:52 +0530

    My 1st Commit to this repo

---
+++ build/precache-manifest.309c1b8d8ef5b083aee82f150f99ff58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b7911b9dad950b0977c",
+    "url": "/static/js/main.6d0fc163.chunk.js"
+  },
+  {
+    "revision": "e5e8a6c77f364b2b27bf",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "4b7911b9dad950b0977c",
+    "url": "/static/css/main.61468884.chunk.css"
+  },
+  {
+    "revision": "e5e8a6c77f364b2b27bf",
+    "url": "/static/css/2.ee4a4d13.chunk.css"
+  },
+  {
+    "revision": "7f07570d7ffa43acc10920e82cee5551",
+    "url": "/index.html"
+  }
+];
---
+++ src/robots.js
@@ -0,0 +1,64 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Leanne Grahan',
+	username: 'Bret',
+	email: 'sincere@april.biz'
+},
+{
+	id: 2,
+	name: 'Tikazo Lena',
+	username: 'Anto',
+	email: 'anto@metika.biz'
+},
+{
+	id: 3,
+	name: 'Pikachu Seso',
+	username: 'Piku',
+	email: 'piku@seso.biz'
+},
+{
+	id: 4,
+	name: 'Clementine Bauch',
+	username: 'Samantha',
+	email: 'bauch@clement.biz'
+},
+{
+	id: 5,
+	name: 'Chelsi Nizo',
+	username: 'Chikasu',
+	email: 'nizo@chelsi.biz'
+},
+{
+
+	id: 6,
+	name: 'Twerty Susie',
+	username: 'Twerty',
+	email: 'twerty@twe.biz'
+},
+{
+	id: 7,
+	name: 'Susie Chink',
+	username: 'Susie',
+	email: 'seso@time.biz'
+},
+{
+	id: 8,
+	name: 'Reaso Grips',
+	username: 'Grips',
+	email: 'grips@april.biz'
+},
+{
+	id: 9,
+	name: 'Telusko Bret',
+	username: 'Telus',
+	email: 'bret@sulet.biz'
+},
+{
+	id: 10,
+	name: 'Grahan Lmao',
+	username: 'Lmao',
+	email: 'lmao@fewo.biz'
+}
+
+]
